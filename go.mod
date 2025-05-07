2136870a0532cb6f1463c845d0bda4d3ba027cb2
Koh Guan Zeh <kohguanzeh@gmail.com>
2025-05-07 11:22:46 +0800

    Add directory info map

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module lsync
 
-go 1.23.3
+go 1.24.3
--- main.go
+++ main.go
@@ -9,58 +9,102 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"time"
 )
 
+type DirInfo struct {
+	files map[string]string
+	dirs  map[string]DirInfo
+}
+
 func main() {
-	dirPath := parseFlags()
-	absDirPath, err := filepath.Abs(dirPath)
+	from, to := parseFlags()
+	from, err := filepath.Abs(from)
 	if err != nil {
 		log.Fatalf("Error getting absolute path: %v\n", err)
 	}
-	start := time.Now()
+	to, err = filepath.Abs(to)
+	if err != nil {
+		log.Fatalf("Error getting absolute path: %v\n", err)
+	}
 	sha := sha1.New()
-	dirWalk(absDirPath, sha, 255)
-	fmt.Printf("Time taken: %s", time.Since(start))
+	fromDirInfo, err := getDirInfo(from, sha)
+	if err != nil {
+		log.Fatalf("Error reading directory: %v\n", err)
+	}
+	toDirInfo, err := getDirInfo(to, sha)
+	if err != nil {
+		log.Fatalf("Error reading directory: %v\n", err)
+	}
+	syncDirs(from, to, fromDirInfo, toDirInfo)
 }
 
 // Parse flags required to run the program.
-func parseFlags() string {
-	dirPathPtr := flag.String("dir", "", "Directory for folder scan.")
-	flag.StringVar(dirPathPtr, "d", *dirPathPtr, "Alias for dir.")
+func parseFlags() (string, string) {
+	fromPathPtr := flag.String("from", "./test-1", "Directory with files to sync from")
+	flag.StringVar(fromPathPtr, "f", *fromPathPtr, "Alias for from")
+	toPathPtr := flag.String("to", "./test-2", "Directory with files to sync to")
+	flag.StringVar(toPathPtr, "t", *toPathPtr, "Alias for to")
 	flag.Parse()
-	return *dirPathPtr
+	return *fromPathPtr, *toPathPtr
 }
 
-// Walk through items in given directory, printing filenames and their SHA1 values.
-func dirWalk(absDirPath string, sha hash.Hash, maxDepth uint8) {
+// Map directory info
+func getDirInfo(absDirPath string, sha hash.Hash) (DirInfo, error) {
+	dirInfo := DirInfo{}
 	items, err := os.ReadDir(absDirPath)
 	if err != nil {
-		fmt.Printf("Error reading directory: %v\n", err)
-		return
+		return dirInfo, err
 	}
-	fmt.Println()
+	dirInfo.files = make(map[string]string)
+	dirInfo.dirs = make(map[string]DirInfo)
 	for _, item := range items {
 		if item.IsDir() {
 			dirname := item.Name()
-			if maxDepth == 0 {
-				fmt.Printf("Encountered directory at max depth: %s\n\n", dirname)
+			subdirInfo, err := getDirInfo(filepath.Join(absDirPath, dirname), sha)
+			if err != nil {
+				fmt.Printf("Error reading directory: %v\n", err)
+				fmt.Println("Skipping...")
 				continue
 			}
-			dirWalk(filepath.Join(absDirPath, dirname), sha, maxDepth-1)
+			dirInfo.dirs[dirname] = subdirInfo
 			continue
 		}
 		filename := item.Name()
-		fmt.Printf("Found file: %s\n", filename)
 		data, err := os.ReadFile(filepath.Join(absDirPath, filename))
 		if err != nil {
 			fmt.Printf("Error reading file: %v\n", err)
+			fmt.Println("Skipping...")
 			continue
 		}
 		sha.Write(data)
 		hexDigest := hex.EncodeToString(sha.Sum(nil)[:8])
 		sha.Reset()
-		// Only show the first 8 bytes of the SHA digest.
-		fmt.Printf("File Contents SHA-1: %s\n\n", hexDigest)
+		dirInfo.files[filename] = hexDigest
 	}
+	return dirInfo, nil
+}
+
+func printDirInfo(dirInfo DirInfo) {
+	if len(dirInfo.files) == 0 {
+		fmt.Println("No files found")
+	}
+	fmt.Println("Files and Content Hash:")
+	for k, v := range dirInfo.files {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+	if len(dirInfo.dirs) == 0 {
+		fmt.Println("No Subdirectories found")
+	}
+	fmt.Println("Subdirectories:")
+	for k, v := range dirInfo.dirs {
+		fmt.Printf("%s:\n", k)
+		printDirInfo(v)
+	}
+}
+
+func syncDirs(from, to string, fromDirInfo, toDirInfo DirInfo) {
+	// TO DO: Sync files from fromDir to toDir
+	printDirInfo(fromDirInfo)
+	fmt.Println()
+	printDirInfo(toDirInfo)
 }
